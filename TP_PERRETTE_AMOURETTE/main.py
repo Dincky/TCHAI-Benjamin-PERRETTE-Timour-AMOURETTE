8d7e3ef53e1d63692f830f19d2277d228231a626
Dincky <83808613+Dincky@users.noreply.github.com>
2022-11-09 12:10:22 +0100

    Début A1 et A2
    Début de l'implémentation de A1 : Enregistrer un transaction et de A2 : l'afficher dans l'ordre chronologique

--- TP_PERRETTE_AMOURETTE/main.py
+++ TP_PERRETTE_AMOURETTE/main.py
@@ -1,13 +1,27 @@
+import datetime
+import random
+
 from flask import *
 
 app = Flask(__name__)
 names = ['Zorro']
 
+transactions = [["ana", "bob", datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S"), str(random.randint(0, 100))],
+                ["paul", "franck", "11/08/2022 11:42:18", str(random.randint(0, 100))],
+                ]
+
+def aff_transaction(transactions):
+    transactions = sorted(transactions, key=lambda i: i[2])
+    return transactions
+
 @app.route('/')
 def hello():
-    return 'Hello <ul>' + ''.join(
-        ['<li> ' + n for n in names]
-    ) + '</ul>\n', 200
+    st = 'Transactions <ul>' + ''.join([])
+    for n in aff_transaction(transactions):
+        st += '<li> '
+        for o in n:
+            st += o + ' '
+    return st + '</ul>\n', 200
 
 
 @app.route('/user/<uname>', methods=['PUT'])
